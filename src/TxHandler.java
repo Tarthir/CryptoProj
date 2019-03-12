fbc69fa1e7a44a41f97aa3811cf960325778a76b
Tarthir <tylerbrady34@gmail.com>
2019-03-12 12:29:50 -0600

    stuff2

--- src/TxHandler.java
+++ src/TxHandler.java
@@ -1,4 +1,7 @@
+import java.security.PublicKey;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class TxHandler {
 
@@ -21,19 +24,55 @@ public class TxHandler {
      *     values; and false otherwise.
      */
     public boolean isValidTx(Transaction tx) {
-        // 1.
         ArrayList<UTXO> utx = new UTXOPool().getAllUTXO();
-        for(Transaction.Output output: tx.getOutputs()){
-            if(utx.contains(output)){
-                //DO something
+        // 1. all outputs claimed by {@code tx} are in the current UTXO pool
+        for (Transaction.Output output : tx.getOutputs()) {
+            if (utx.contains(output)) {
+                //3. no UTXO is claimed multiple times by {@code tx}
+                Set<Transaction.Output> set_list = new LinkedHashSet<>(tx.getOutputs());
+                if (set_list.size() < tx.getOutputs().size()) {
+                    return false
+                }
                 return true;
-            }
-            else{
+            } else {
                 return false;
             }
 
         }
+        //2 the signatures on each input of {@code tx} are valid
+        for (int i = 0; i < tx.getInputs().size(); i++) {
+            PublicKey publicKey = tx.getOutput(tx.getInput(i).outputIndex).address;
+            byte[] message = tx.getRawDataToSign(i);
+            byte[] signature = tx.getInput(i).signature;
+            if (!Crypto.verifySignature(publicKey, message, signature)) {
+                return false;
+            }
+        }
+        // 4 all of {@code tx}s output values are non-negative
+        for (Transaction.Output output : tx.getOutputs()) {
+            if (output.value < 0) {
+                return false;
+            }
+        }
+
+        // 5. the sum of {@code tx}s input values is greater than or equal to the sum of its output
+        //  values; and false otherwise.
+        double inputSum = 0;
+        double outputSum = 0;
+        for (Transaction.Input input : tx.getInputs()) {
+            Transaction.Output prevOutput = tx.getOutput(input.outputIndex);
+            inputSum += prevOutput.value;
+        }
+        for (Transaction.Output output : tx.getOutputs()) {
+            outputSum += output.value;
+        }
+        if (outputSum < inputSum) {
+            return false;
+        }
+
+        return true;
     }
+
 
     /**
      * Handles each epoch by receiving an unordered array of proposed transactions, checking each
