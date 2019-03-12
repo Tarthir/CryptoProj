2329e5b3c8d1812ec81345703d38c23ac69d83c0
Tarthir <tylerbrady34@gmail.com>
2019-03-12 12:44:18 -0600

    Need to test now

--- src/TxHandler.java
+++ src/TxHandler.java
@@ -1,9 +1,11 @@
 import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 public class TxHandler {
+    private UTXOPool utxoPool;
 
     /**
      * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
@@ -11,30 +13,29 @@ public class TxHandler {
      * constructor.
      */
     public TxHandler(UTXOPool utxoPool) {
-        // IMPLEMENT THIS
+        this.utxoPool = utxoPool;
     }
 
     /**
      * @return true if:
-     * (1) all outputs claimed by {@code tx} are in the current UTXO pool, 
-     * (2) the signatures on each input of {@code tx} are valid, 
+     * (1) all outputs claimed by {@code tx} are in the current UTXO pool,
+     * (2) the signatures on each input of {@code tx} are valid,
      * (3) no UTXO is claimed multiple times by {@code tx},
      * (4) all of {@code tx}s output values are non-negative, and
      * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
-     *     values; and false otherwise.
+     * values; and false otherwise.
      */
     public boolean isValidTx(Transaction tx) {
-        ArrayList<UTXO> utx = new UTXOPool().getAllUTXO(); //TODO need a utxo pool that is stored somewhere/static
+        ArrayList<UTXO> utx = this.utxoPool.getAllUTXO(); //TODO need a utxo pool that is stored somewhere/static
         // 1. all outputs claimed by {@code tx} are in the current UTXO pool
         for (Transaction.Output output : tx.getOutputs()) {
             if (utx.contains(output)) {
                 //3. no UTXO is claimed multiple times by {@code tx}
                 Set<Transaction.Output> set_list = new LinkedHashSet<>(tx.getOutputs());
                 if (set_list.size() < tx.getOutputs().size()) {
-                    return false
+                    return false;
                 }
-            }
-            else {
+            } else {
                 return false;
             }
 
@@ -80,7 +81,15 @@ public class TxHandler {
      * updating the current UTXO pool as appropriate.
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
-        // IMPLEMENT THIS
+        List<Transaction> transactions = new ArrayList<>();
+        for(int i = 0; i < possibleTxs.length; i++) {
+            if(isValidTx(possibleTxs[i])) {
+                transactions.add(possibleTxs[i]);
+                for(Transaction.Output output : possibleTxs[i].getOutputs()) {
+                    utxoPool.addUTXO(new UTXO(possibleTxs[i].getHash(), i), output);
+                }
+            }
+        }
+        return (Transaction[])transactions.toArray();
     }
-
 }
